f3541696f9545f057fcee1eba652289df2e8ff15
Sheikh Mohammed Shamnooon <sheikh@finaskus.com>
2016-10-13 16:58:15 +0530

    Added folder structure

---
+++ app/app.js
@@ -0,0 +1,65 @@
+/*
+Copyright Â© 2016, FinAskUs
+Written under contract by Robosoft Technologies Pvt. Ltd.
+*/
+(function(){
+	'use strict';
+	angular.module('finApp',[
+		'ngRoute',
+		'ngResource',
+		'ngSanitize',
+	])
+	.config(config)
+	.run(run);
+
+	config.$inject = ['$routeProvider','$httpProvider'];
+	function config($routeProvider,$httpProvider){
+		$routeProvider
+			.when('/', {
+                templateUrl: 'modules/authentication/views/login.html',
+                controller: 'authController'
+            })
+            .otherwise({
+                redirectTo: '/404',
+                title : 'Page not found',
+                templateUrl: 'modules/common/views/404.html'
+            });
+		$httpProvider.interceptors.push('finAppHttpIntercepter');
+	}
+	run.$inject = ['$route','$routeParams','$rootScope',];
+	function run($route,$routeParams,$rootScope){
+		
+		
+		$rootScope.$on('$locationChangeStart', function(event, current, previous) {
+		
+		});
+		$rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+			$rootScope.title = $route.current.title;
+
+		});
+		
+		$rootScope.userLogout = function(){
+
+		}
+
+		$rootScope.$on('userloggedIn', function(event, data) {
+
+		});
+        $rootScope.$on('userloggedOut', function(event, args) {
+     
+        });
+
+		document.addEventListener("online", function() { 
+			$rootScope.online = true;  
+		}, false);
+
+		document.addEventListener("offline", function() { 
+			$rootScope.online = false;   
+		}, false);
+
+		$rootScope.$watch('online', function(newStatus) {
+            if (newStatus == false) {
+            }
+        });
+	}
+})();
---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+var gulp = require('gulp'),
+server = require('gulp-server-livereload'),
+less = require('gulp-less'),
+watch = require('gulp-watch'),
+concat = require('gulp-concat'),
+htmlmin = require('gulp-htmlmin'),
+rename = require('gulp-rename'),  
+uglify = require('gulp-uglify'), 
+exec = require('gulp-exec'),
+runSequence = require('run-sequence'), 
+inject = require('gulp-inject'),
+LessPluginCleanCSS = require('less-plugin-clean-css'),
+LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+cleancss = new LessPluginCleanCSS({ advanced: true }),
+autoprefix = new LessPluginAutoPrefix({ 
+	browsers: ["> 0%"]
+});
+
+//var imagemin = require('gulp-imagemin');
+
+
+gulp.task('less', function () {
+	return gulp.src('less/main.less')
+		  .pipe(less({
+		    plugins: [autoprefix]
+		  }))
+		.pipe(gulp.dest('app/assets/css'));
+});
+
+gulp.task('serve',['watch'], function() {
+  gulp.src('app/')
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('less/*.less', ['less']);
+});
+
+
+//script paths
+var jsFilesModules = 'app/modules/**/*.js',  
+	jsFilesJSlibs = 'app/lib/*.js',
+    jsFilesCsslibs = 'app/lib/*.css',
+    assets = 'app/assets/**',
+    mainJs = 'app/app.js',
+    jsDest = 'dist',
+    devePlace = 'app/**';
+ 
+gulp.task('minImages', function(){
+     gulp.src('app/assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/assets/images'))
+});
+
+
+gulp.task('copyAssets', function() {  
+    return gulp.src(assets)
+        .pipe(gulp.dest(jsDest+'/assets'));
+});
+
+gulp.task('copylibCssFiles', function() {  
+    return gulp.src(jsFilesCsslibs)
+        .pipe(gulp.dest(jsDest+'/lib'));
+});
+
+gulp.task('copylibJSFiles', function() {  
+    return gulp.src(jsFilesJSlibs)
+        .pipe(gulp.dest(jsDest+'/lib'));
+});
+
+gulp.task('minifyJS', function() {  
+    return gulp.src([mainJs,jsFilesModules])
+        .pipe(concat('vendor.js'))
+        .pipe(rename('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('copyIndex', function() {
+    return gulp.src('app/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyHTML',['copyIndex'], function() {
+    return gulp.src('app/modules/**/views/**')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist/modules'));
+});
+
+gulp.task('giveBuild',['copyAssets','minifyJS','copylibJSFiles','copylibCssFiles','copyIndex','copyHTML']);
+
+gulp.task('copyFiles',function(){
+    return gulp.src(devePlace)
+    .pipe(gulp.dest('../CombatPhoneGap/www/'));
+});
+
+var exec = require('child_process').exec;
+
+gulp.task('cordova', function(done) {
+  exec('corodva run', { cwd: '../CombatPhoneGap', stdio: 'inherit' })
+    .on('close', done);
+});
+
+gulp.task('build',['copyFiles','cordova']);
+
