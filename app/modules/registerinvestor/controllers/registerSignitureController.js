3408dc0f02ce815380c9ffbbed51c6c62e091e3b
Sheikh Mohammed Shamnooon <sheikh@finaskus.com>
2016-12-08 18:32:19 +0530

    Added Signiture functionality

---
+++ app/modules/registerinvestor/controllers/registerSignitureController.js
@@ -0,0 +1,16 @@
+(function(){
+	'use strict';
+	angular
+		.module('finApp.registerInvestor')
+		.controller('registerSignatureController',registerSignatureController);
+
+		registerSignatureController.$inject = ['$rootScope','$scope','$http','registerInvestorService'];
+		function registerSignatureController($rootScope,$scope,$http,registerInvestorService){
+			$scope.showSigniture = function(){
+				$('#signitureModal').modal('show');
+			}
+			$scope.done = function (){
+				var signature = $scope.accept();
+			}
+		}
+})();
