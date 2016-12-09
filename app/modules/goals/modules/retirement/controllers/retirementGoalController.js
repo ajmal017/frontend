9ea2ace9d8a887de37a98f8a8f26a21957f96b60
Rashmi Mittal <rashmi@finaskus.com>
2016-12-09 14:56:49 +0530

    Hook up Corpus and SIP calculation

--- app/modules/goals/modules/retirement/controllers/retirementGoalController.js
+++ app/modules/goals/modules/retirement/controllers/retirementGoalController.js
@@ -4,8 +4,10 @@
 		.module('finApp.goals')
 		.controller('retirementGoalsController',retirementGoalsController);
 
-		retirementGoalsController.$inject = ['$scope','$rootScope','$route','$location','goalsService','retirementGoalsService']
-		function retirementGoalsController($scope,$rootScope,$route,$location,goalsService,retirementGoalsService){
+		retirementGoalsController.$inject = ['$scope','$rootScope','$route','$location',
+		                                     'goalsService','retirementGoalsService', 'assetAllocationService', 'goalFormulaeService']
+		function retirementGoalsController($scope,$rootScope,$route,$location,goalsService,retirementGoalsService, 
+											assetAllocationService, goalFormulaeService) {
 			$scope.retirement = {};
 			$scope.modelVal = retirementGoalsService.getSavedValues();
 			$scope.showEquityModal = function(){
@@ -23,7 +25,53 @@
 			$scope.calculateYear = function(from,to){
 				var currentYear = new Date();				
 				$scope.retirement['calculateYear'] = currentYear.getFullYear() + (to - from);
+				$scope.retirement['tenure'] = to - from;
 			}
+			$scope.getAssetAllocationCategory = function(){
+				var currentYear = new Date(),
+					tenure = $scope.retirement['tenure'];
+				
+				assetAllocationService.computeAssetAllocationCategory(tenure).then(function(data){
+					if('success' in data){
+						$scope.retirement['assetAllocationCategory'] = data.success['asset_allocation_category'];
+					}
+					else {
+						$scope.retirement['assetAllocationCategory'] = "A"; //TODO define constants, default category
+					}
+					$scope.broadcast('assetAllocationCategoryChanged');
+					if(!$scope.$$phase)	$scope.$apply();
+				});
+
+			}
+			$scope.calculateRecommendedSIP = function(corpus) {
+				var calculateSIP - function() {
+					$scope.retirement['perMonth'] = goalFormulaeService.computeSIPForCorpus({'corpus': corpus, 'tenure': $scope.retirement['tenure'] }, $scope.retirement['assetAllocationCategory']);
+					if(!$scope.$$phase)	$scope.$apply();
+				};
+				
+				if (!$scope.retirement['assetAllocationCategory']) {
+					$scope.$on('assetAllocationCategoryChanged', calculateSIP);
+				}
+				else {
+					calculateSIP();
+				}
+			}
+
+			$scope.calculateCorpus = function(sipAmount) {
+				var calculateCorpus - function() {
+					goalFormulaeService.computeSIPForCorpus({'sip': sipAmount, 'tenure': $scope.retirement['tenure'] }, $scope.retirement['assetAllocationCategory']);
+					if(!$scope.$$phase)	$scope.$apply();
+				};
+				
+				if (!$scope.retirement['assetAllocationCategory']) {
+					$scope.$on('assetAllocationCategoryChanged', calculateCorpus);
+				}
+				else {
+					calculateCorpus();
+				}
+			}
+
 			$scope.graphObject = goalsService.getGoalGraphDetails();
 		}
+
 })();
