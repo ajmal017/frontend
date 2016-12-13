311ba51b6fd2529f12e2da81742bfbde90bd81bd
Rashmi Mittal <rashmi@finaskus.com>
2016-12-13 17:10:58 +0530

    Added calculations for wedding and vacation goals

--- app/modules/goals/modules/wedding/services/weddingService.js
+++ app/modules/goals/modules/wedding/services/weddingService.js
@@ -7,11 +7,43 @@
         weddingService.$inject = ['$resource','$rootScope','appConfig','$q'];
         function weddingService($resource,$rootScope,appConfig,$q){
         	
+        	var modelObject = {};
+        	
             return{
-        		getPlanDetails : getPlanDetails
+            	getSavedValues : getSavedValues,
+            	setSavedValues : setSavedValues,
+        		getCorpusEstimates : getCorpusEstimates
         	}
 
-	        function getPlanDetails(){               
+	        function getSavedValues(){
+	        	return modelObject;
 	        }
+
+	        function setSavedValues(){               
+	        }
+
+            function getCorpusEstimates(term, expectedPeople, location, sharingPercentage, amountSaved) {
+            	var queryData = {"term":term, "expected_people":expectedPeople, "sharing_percentage":sharingPercentage, "location" : location, "amount_saved": amountSaved};
+        		var defer = $q.defer();
+				var getAPI = $resource( 
+					appConfig.API_BASE_URL+'/core/goal/wedding/estimate/', 
+					{}, {
+						Check: {
+							method:'GET',
+						}
+					});
+				getAPI.Check({"data" : queryData},function(data){
+					if(data.status_code == 200){
+						defer.resolve({'success':data.response});
+					}else{
+						defer.resolve({'Message':data.response['message']});
+					}				
+				}, function(err){
+					defer.reject(err);
+				}); 
+				return defer.promise;
+
+            }
+
         }     
 })();
