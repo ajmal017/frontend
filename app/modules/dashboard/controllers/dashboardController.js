4d25d65589c4640b81c34cd4fb3187f98b1398f0
Shamnoon <sheikh@finaskus.com>
2016-12-23 17:03:33 +0530

    Made dashboard dynamic

--- app/modules/dashboard/controllers/dashboardController.js
+++ app/modules/dashboard/controllers/dashboardController.js
@@ -9,5 +9,32 @@
 			// dashboardService.getDashboardDetails($rootScope.userFlags,function(data){
 			// 	$scope.dashCounts = data;
 			// })
+
+			var userFlags = JSON.parse(sessionStorage.getItem('userFlags'));
+			$scope.risk_score = userFlags.user_answers.risk_score;
+			if($scope.risk_score > 0 && $scope.risk_score < 4) {
+	            	
+            	$scope.riskType = 'Low Risk';
+            } else if($scope.risk_score >= 4.1 && $scope.risk_score <= 6.0) {
+            	
+            	$scope.riskType = 'Below Average Risk';      	
+            } else if($scope.risk_score >= 6.1 && $scope.risk_score <= 7.5) {
+            	
+            	$scope.riskType = 'Average Risk';      	
+            	
+            } else if($scope.risk_score >= 7.6 && $scope.risk_score <= 9.0) {
+            	
+            	$scope.riskType = 'Above Average Risk';      	
+            	
+            } else if($scope.risk_score >= 9.1 && $scope.risk_score <= 10.0) {
+            	
+            	$scope.riskType = 'High Risk';
+            }
+
+            $scope.user_flags = userFlags.user_flags;
+            $scope.portfolio_flag = userFlags.user_flags.portfolio;
+			$rootScope.userRiskFactor = userFlags.user_answers.risk_score;
+			console.log('risk_score',userFlags.user_answers.risk_score);
+
 		}
 })();
