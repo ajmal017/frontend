d69b07cfba7ad37d26c9c01eb8d72d924bde9c21
Rashmi Mittal <rashmi@finaskus.com>
2017-01-04 11:21:36 +0530

    Google Login refactored to work with Gooogle Signin button in registration screen

--- app/modules/authentication/controllers/authController.js
+++ app/modules/authentication/controllers/authController.js
@@ -7,156 +7,45 @@
 		authController.$inject = ['$scope','$rootScope','$location','userDetailsService','authService','registerService','GoogleSignin','ngDialog']
 		function authController($scope,$rootScope,$location,userDetailsService,authService,registerService,GoogleSignin,ngDialog){
 			
-			$scope.completeLogin = function(data) {
-				if('success' in data){
-					authService.submitSuccess(data).then(function(data){
-						userDetailsService().then(function(userData){
-							$rootScope.$broadcast('refreshCredentials',userData['success']);
-							$location.path('/dashboard');
-						});
-					});		
-				}else{
-					$scope.errorMessage = data['Message'] + " " + data['Error'];
-				}
-			}
+			this.scope = $scope;
+
+			this.rootScope = $rootScope;
+			this.location = $location;
+			this.userDetailsService = userDetailsService;
+			this.authService = authService;
+			this.registerService = registerService;
+			this.GoogleSignin = GoogleSignin;
+			this.ngDialog = ngDialog;			
 			
+			var self = this;
+
+			this.scope.completeLogin = angular.bind( this, this.completeLogin );
+			
+			this.scope.completeGoogleLogin = angular.bind( this, this.completeGoogleLogin );
+
+			this.scope.registerPhone = angular.bind( this, this.registerPhone );
+
+			this.scope.processGoogleRegister = angular.bind( this, this.processGoogleRegister );
+
+			this.scope.mergeAccounts = angular.bind( this, this.mergeAccounts );
+
+			this.scope.cancelGoogleAccountMerge = angular.bind( this, this.cancelGoogleAccountMerge );
+
+			this.scope.confirmGoogleAccountMerge = angular.bind( this, this.confirmGoogleAccountMerge );
+
+			this.scope.processGoogleAccountMerge = angular.bind( this, this.processGoogleAccountMerge );
+
+			this.scope.googleSignIn = angular.bind( this, this.googleSignIn );
+
+			this.scope.googleRegisterResendOtp = angular.bind( this, this.googleRegisterResendOtp );
+
+			this.scope.googleRegisterConfirmOtp = angular.bind( this, this.googleRegisterConfirmOtp );
+
 			$scope.verifyLogin = function(user){
 				authService.verifyLogin(user).then(function(data){
 					$scope.completeLogin(data);
 				});
 			}
-
-			$scope.completeGoogleLogin = function() {
-				$('#otpModal').modal('hide');
-				$scope.completeLogin($scope.registerSuccessData);
-			}
-			
-			$scope.registerPhone = function(mobileNumber) {
-				if (mobileNumber) {
-					$scope.googleAuthParams['phone_number'] = mobileNumber;
-					authService.googleRegister($scope.googleAuthParams).then(function(data) {
-						if ('success' in data) {
-							ngDialog.closeAll();
-							$scope.registerSuccessData = data;
-							$('#otpModal').modal('show');
-						}
-						else {
-							$scope.errorPhone = data['Message'] + " " + data['Error'];
-						}
-					});
-				}
-			}
-
-			$scope.processGoogleRegister = function() {
-				$scope.ngDialog = ngDialog;
-				ngDialog.open({ 
-		        	template: 'modules/authentication/views/google_registration.html', 
-		        	className: 'goal-ngdialog-overlay ngdialog-theme-default',
-		        	overlay: false,
-		        	showClose : false,
-
-		        	scope: $scope
-	        	});
-			}
-			
-			$scope.mergeAccounts = function(user) {
-				$scope.googleAuthParams['password'] = user.password;
-				authService.googleRegisterExistingUser($scope.googleAuthParams).then(function(data) {
-					if ('success' in data) {
-						ngDialog.closeAll();
-						$scope.completeLogin(data);
-					}
-					else {
-						$scope.errorMessage = data['Message'] + " " + data['Error'];
-					}
-
-				});
-			}
-			
-			$scope.cancelAccountMerge = function() {
-				ngDialog.closeAll();
-			}
-			
-			$scope.confirmAccountMerge = function() {
-				ngDialog.closeAll();
-				
-		        ngDialog.open({ 
-		        	template: 'modules/authentication/views/confirmMergeLogin.html', 
-		        	className: 'goal-ngdialog-overlay ngdialog-theme-default',
-		        	overlay: false,
-		        	showClose : false,
-
-		        	scope: $scope
-		        });
-			}
-			$scope.processGoogleAccountMerge = function() {
-				$scope.modalMessage = 'You have an existing FinAskus account with the same Google email.\r Your Google account will be merged with the FinAskus account. \r\n Please proceed to validate your existing FinAskus account.';
-		        ngDialog.open({ 
-		        	template: 'modules/authentication/views/confirmMerge.html', 
-		        	className: 'goal-ngdialog-overlay ngdialog-theme-default',
-		        	overlay: false,
-		        	showClose : false,
-
-		        	scope: $scope
-		        });
-
-			}
-			
-			$scope.googleSignIn = function(){
-				GoogleSignin.signIn().then(function (user) {
-					var authResponse = user.getAuthResponse(),
-					profile = user.getBasicProfile();
-					GoogleSignin.grantOfflineAccess({'authuser':authResponse.session_state.extraQueryParams.authuser, 
-						'scope': 'profile email'}).then(function(authResult) {
-						var accessToken = authResult.code;
-					
-						$scope.googleAuthParams = {'auth_code':accessToken, 'email': profile.getEmail(), 'is_web': true,
-													'image':profile.getImageUrl()};
-						authService.googleLogin($scope.googleAuthParams).then(function(data) {
-							if('success' in data){
-								var userStatus = data['success']['user_status'];
-								
-								if (userStatus == 2) {
-									$scope.completeLogin({'success':data['success']['res']});
-								}
-								else if (userStatus == 3) {
-									$scope.processGoogleRegister();
-								}
-								else if (userStatus == 4) {
-									$scope.processGoogleAccountMerge();
-								}
-							}
-							else {
-								$scope.errorMessage = data['Message'] + " " + data['Error'];
-							}
-						});
-						
-						console.log(JSON.stringify(user));
-
-					});
-		        }, function (err) {
-		            console.log(JSON.stringify(err));
-		        });
-			}
-			
-			$scope.resendOtp = function(){
-				registerService.resendOtp($scope.registerSuccessData['success']['tokens']).then(function(data){
-					alert(JSON.stringify(data));
-				})
-			}
-
-			$scope.confirmOtp = function(otp){
-				registerService.confirmOtp(otp,$scope.registerSuccessData['success']['tokens']).then(function(data){
-					if('success' in data){
-						$('#otpModal').modal('hide');
-						setTimeout(function(){
-							$scope.completeLogin($scope.registerSuccessData);
-						},1000);						
-					}else{
-						$scope.errorOtp = data['Message'];
-					}
-				})
-			}
-
 		}
+		authController.prototype = finApp.authControllerPrototype;
 })();
