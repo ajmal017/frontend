9d5b52d4bbaa5ed705902186c550740395bde391
Rashmi Mittal <rashmi@finaskus.com>
2017-02-10 09:28:12 +0530

    Fix Google Login on safari

--- app/modules/authentication/controllers/authControllerPrototype.js
+++ app/modules/authentication/controllers/authControllerPrototype.js
@@ -104,43 +104,68 @@ var finApp = finApp || {};
 			},
 			
 			googleSignIn : function(){
-				var self = this;
-				this.GoogleSignin.signIn().then(function (user) {
-					var authResponse = user.getAuthResponse(),
-					profile = user.getBasicProfile();
-					self.GoogleSignin.grantOfflineAccess({'authuser':authResponse.session_state.extraQueryParams.authuser, 
-						'scope': 'profile email'}).then(function(authResult) {
-						var accessToken = authResult.code;
-					
-						self.scope.googleAuthParams = {'auth_code':accessToken, 'email': profile.getEmail(), 'is_web': true,
-													'image':profile.getImageUrl()};
-						self.authService.googleLogin(self.scope.googleAuthParams).then(function(data) {
-							if('success' in data){
-								var userStatus = data['success']['user_status'];
-								
-								if (userStatus == 2) {
-									self.scope.completeLogin({'success':data['success']['res']});
-								}
-								else if (userStatus == 3) {
-									self.scope.processGoogleRegister();
-								}
-								else if (userStatus == 4) {
-									self.scope.processGoogleAccountMerge();
-								}
+				var self = this,
+					accessToken, profile;
+				
+				var doLogin = function() {
+					self.scope.googleAuthParams = {'auth_code':accessToken, 'email': profile.getEmail(), 'is_web': true,
+							'image':profile.getImageUrl()};
+					self.authService.googleLogin(self.scope.googleAuthParams).then(function(data) {
+						if('success' in data){
+							var userStatus = data['success']['user_status'];
+							
+							if (userStatus == 2) {
+								self.scope.completeLogin({'success':data['success']['res']});
 							}
-							else {
-								self.scope.errorMessage = data['Message'] + " " + data['Error'];
+							else if (userStatus == 3) {
+								self.scope.processGoogleRegister();
 							}
-						});
-						
-						console.log(JSON.stringify(user));
-
+							else if (userStatus == 4) {
+								self.scope.processGoogleAccountMerge();
+							}
+						}
+						else {
+							self.scope.errorMessage = data['Message'] + " " + data['Error'];
+						}
 					});
-		        }, function (err) {
-		            console.log(JSON.stringify(err));
-		        });
+				};
+				
+				var isSafari = (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1 && navigator.userAgent.indexOf('Android') == -1);
+				
+				if (isSafari) {
+					self.GoogleSignin.grantOfflineAccess({'scope' : 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile', 
+						'scopes' : 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile', 'redirect_uri': 'postmessage',
+						'approval_prompt' : 'force'}).then(function(authResult) {
+							accessToken = authResult.code;
+							var user = self.GoogleSignin.getUser();
+							profile = user.getBasicProfile();
+							
+							if (profile) {
+								doLogin();
+							}	
+						}, function (err) {
+							console.log(err);
+						});
+				}
+				else {
+					self.GoogleSignin.signIn().then(function (user) {
+						var authResponse = user.getAuthResponse();
+						
+						profile = user.getBasicProfile();
+						self.GoogleSignin.grantOfflineAccess({'authuser':authResponse.session_state.extraQueryParams.authuser, 
+							'scopes' : 'https://www.googleapis.com/auth/userinfo.email', 'redirect_uri': 'postmessage'}).then(function(authResult) {
+							accessToken = authResult.code;
+							
+							doLogin();
+						}, function (err) {
+							console.log(err);
+						});
+			        }, function (err) {
+			            console.log(JSON.stringify(err));
+			        });
+				}
 			},
-			
+
 			googleRegisterResendOtp : function(){
 				this.registerService.resendOtp(this.scope.registerSuccessData['success']['tokens']).then(function(data){
 					alert(JSON.stringify(data));
